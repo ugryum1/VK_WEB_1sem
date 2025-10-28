6b355b51d8357ebcb5d0cc0cdf8ec030746ea8f7
ugryum1 <n.a.ugryumov@gmail.com>
2025-10-28 14:53:47 +0300

    Updste question app and add core templates

--- core/views.py
+++ core/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
 
+TOPUSERS = [
+    {
+        "rank": 1,
+        "username": "Dr. Pepsi"
+    },
+    {
+        "rank": 2,
+        "username": "Secret User"
+    },
+    {
+        "rank": 3,
+        "username": "Carte Blanche"
+    }
+]
+
+TOPTAGS = [
+    {
+        "id": 1,
+        "tag": "Python"
+    },
+    {
+        "id": 2,
+        "tag": "PostgreSQL"
+    },
+    {
+        "id": 3,
+        "tag": "Django"
+    },
+    {
+        "id": 4,
+        "tag": "VK"
+    },
+    {
+        "id": 5,
+        "tag": "Mail.ru"
+    },
+    {
+        "id": 6,
+        "tag": "Go"
+    },
+    {
+        "id": 7,
+        "tag": "C++"
+    }
+]
+
 def login(request, *args, **kwargs):
     return render(request, 'core/login.html')
 
@@ -7,4 +53,4 @@ def register(request, *args, **kwargs):
     return render(request, 'core/register.html')
 
 def settings(request, *args, **kwargs):
-    return render(request, 'core/settings.html')
+    return render(request, 'core/settings.html', context={"topUsers" : TOPUSERS, "topTags": TOPTAGS})
