2815e05c052421d966cdc33184c7a866f6e7c66a
ugryum1 <n.a.ugryumov@gmail.com>
2025-10-31 11:36:09 +0300

    Update project files

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from core.views import login, register, settings
 
+app_name = 'core'
+
 urlpatterns = [
-    path('login/', login),
-    path('signup/', register),
-    path('settings/', settings),
+    path('login/', login, name='login'),
+    path('signup/', register, name='signup'),
+    path('settings/', settings, name='settings'),
 ]
--- questions/urls.py
+++ questions/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from questions.views import index, question, ask, tag, top
 
+app_name = 'questions'
+
 urlpatterns = [
-    path('', index),
-    path('question/35/', question),
-    path('ask/', ask),
-    path('tag/blablabla/', tag),
-    path('hot/', top),
+    path('', index, name='main_page'),
+    path('question/35/', question, name='question'),
+    path('ask/', ask, name='ask'),
+    path('tag/blablabla/', tag, name='tag'),
+    path('hot/', top, name='hot'),
 ]
