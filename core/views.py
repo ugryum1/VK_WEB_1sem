19052980246e9c28e704e3771e2cb05e6b1f001e
ugryum1 <n.a.ugryumov@gmail.com>
2025-11-03 18:01:15 +0300

    Update template project

--- core/views.py
+++ core/views.py
@@ -1,50 +1,5 @@
 from django.shortcuts import render
-
-TOPUSERS = [
-    {
-        "rank": 1,
-        "username": "Dr. Pepsi"
-    },
-    {
-        "rank": 2,
-        "username": "Secret User"
-    },
-    {
-        "rank": 3,
-        "username": "Carte Blanche"
-    }
-]
-
-TOPTAGS = [
-    {
-        "id": 1,
-        "tag": "Python"
-    },
-    {
-        "id": 2,
-        "tag": "PostgreSQL"
-    },
-    {
-        "id": 3,
-        "tag": "Django"
-    },
-    {
-        "id": 4,
-        "tag": "VK"
-    },
-    {
-        "id": 5,
-        "tag": "Mail.ru"
-    },
-    {
-        "id": 6,
-        "tag": "Go"
-    },
-    {
-        "id": 7,
-        "tag": "C++"
-    }
-]
+from fictional_data import TOPUSERS, TAGS
 
 def login(request, *args, **kwargs):
     return render(request, 'core/login.html')
@@ -53,4 +8,4 @@ def register(request, *args, **kwargs):
     return render(request, 'core/register.html')
 
 def settings(request, *args, **kwargs):
-    return render(request, 'core/settings.html', context={"topUsers" : TOPUSERS, "topTags": TOPTAGS})
+    return render(request, 'core/settings.html', context={"top_users" : TOPUSERS, "top_tags": TAGS})
