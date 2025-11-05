197e20414a30fa69ef6422d7f5d41cb622930be4
ugryum1 <n.a.ugryumov@gmail.com>
2025-11-06 00:18:52 +0300

    Add Docker and remove __pycache from repo

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+# Копируем зависимости first для кэширования
+COPY requirements.txt .
+
+# Устанавливаем Python зависимости
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Копируем весь проект
+COPY . .
+
+# Открываем порт
+EXPOSE 8000
+
+# Запускаем сервер
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
